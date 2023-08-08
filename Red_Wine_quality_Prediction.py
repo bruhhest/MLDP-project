fc22db1a0cdc441338ab3738ed2f8619a2972108
bruhhest <141618479+bruhhest@users.noreply.github.com>
2023-08-08 10:22:04 +0800

    Update Red_Wine_quality_Prediction.py

--- Red_Wine_quality_Prediction.py
+++ Red_Wine_quality_Prediction.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Simple Red Wine Quality Prediction App
